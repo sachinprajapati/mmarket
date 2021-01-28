663f48961d023b3a4eca7c930b615d6e901fca8a
sachin <tavarsachin@gmail.com>
2021-01-28 17:01:49 +0530

    admin register

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Address
+from .models import *
 
 admin.site.register(Address)
+admin.site.register(Orders)
+admin.site.register(OrderItems)
+admin.site.register(OrderPayment)
